5210f741496c0f99eac7e32ecd2b0aaa32347ac7
harsh <harspemare5@gmail.com>
2025-07-02 19:28:02 +0530

    added profile and some decorators so that authentication can be done easily without any trouble

--- routes.py
+++ routes.py
@@ -11,15 +11,7 @@ from models import User, db
 
 # for security important stuff from  werkzeug.security
 from werkzeug.security import generate_password_hash,check_password_hash
-
-@app.route('/')
-def index():
-    #check if the user is in the session or not(we can say that it is a cookie) this will not allow us to enter in the index page directly
-    if 'user_id' in session:
-        return render_template("index.html")
-    else :
-        flash('please login first')
-        return redirect(url_for('login'))
+from functools import wraps
 
 @app.route('/login')
 def login():
@@ -82,4 +74,67 @@ def register_post():
     user=User(username=username,passhash=password,name=name)
     db.session.add(user)
     db.session.commit()
+    return redirect(url_for('login'))
+
+#adding thing called decorator such that it will add additional functionality in our code so that we does not need to authenticate many times 
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user_id' not in session:
+            flash('Please login first')
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+#Create a Profile of a user which will show the user about it's own self
+@app.route('/profile')
+@login_required
+def profile():
+        #fetch the user id from here
+        user=User.query.get(session['user_id'])
+        return render_template('profile.html',user=user)
+
+@app.route('/profile',methods=['POST'])
+@login_required
+def profile_post():
+        #fetch the user id from here
+        username=request.form.get('username')
+        curent_password=request.form.get('password')
+        new_password=request.form.get('new_password')
+        name=request.form.get('name')
+        
+        if not username or not curent_password or not new_password:
+            flash('Please fill out all the necessaryu fields')
+            return redirect(url_for('profile'))
+        
+        user=User.query.get(session['user_id'])
+        if not check_password_hash(user.passhash,curent_password):
+            flash('Enter correct current password')
+            return redirect(url_for('profile'))
+        
+        if username !=user.username:
+            new_username=User.query.filter_by(username=username).first()
+            if new_username:
+                flash('username already exists')
+                return redirect(url_for('profile'))
+            user.username=username
+            passhash=generate_password_hash(new_password)
+            user.passhash=passhash
+            user.name=name
+            db.session.commit()
+            flash('changes are made successfully')
+            return redirect(url_for('login'))
+
+@app.route('/')
+@login_required
+def index():
+    
+    #check if the user is in the session or not(we can say that it is a cookie) this will not allow us to enter in the index page directly
+    return render_template("index.html")
+
+#adding logout button in here
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('user_id')
     return redirect(url_for('login'))
